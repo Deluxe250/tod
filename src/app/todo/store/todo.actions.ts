850cdf5a14f5f4c2bbb94eda40d9349189eb7fd9
Sergey Butuzov <sergey.butuzov@idp.zyfra.com>
2024-09-05 16:44:41 +0400

    3

--- src/app/todo/store/todo.actions.ts
+++ src/app/todo/store/todo.actions.ts
@@ -2,7 +2,6 @@ import { createAction, props } from '@ngrx/store';
 
 export const check = createAction(
   '[TodoList Component] Check',
-
   props<{ todoId: string }>()
 );
 export const uncheck = createAction(
