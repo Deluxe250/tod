50c249e8a64b56428d8178b244e9f45f7b90cdab
Sergey Butuzov <sergey.butuzov@idp.zyfra.com>
2024-09-05 16:48:05 +0400

    4

--- src/app/todo/store/todo.actions.ts
+++ src/app/todo/store/todo.actions.ts
@@ -4,6 +4,7 @@ export const check = createAction(
   '[TodoList Component] Check',
   props<{ todoId: string }>()
 );
+
 export const uncheck = createAction(
   '[TodoList Component] Uncheck',
   props<{ todoId: string }>()
